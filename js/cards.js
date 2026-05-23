5d6ce1ab983ed2868c1dec8a877ab89cf94debc6
Helpfulman2003 <thun23872@gmail.com>
2026-05-23 18:02:31 +0700

    Update blackjack assets and neon theme

--- js/cards.js
+++ js/cards.js
@@ -1,319 +1,48 @@
-//Full array of possible cards
+// Full array of possible cards built from the Raiku art set
 var cards;
 
-function getCards(){
-	cards = [
-		{ 
-			"suit": "clubs",
-			"name": "2",
-			"src": "Clubs-2.png",
-			"value": 2
-		},
-		{ 
-			"suit": "clubs",
-			"name": "3",
-			"src": "Clubs-3.png",
-			"value": 3
-		},
-		{ 
-			"suit": "clubs",
-			"name": "4",
-			"src": "Clubs-4.png",
-			"value": 4
-		},
-		{ 
-			"suit": "clubs",
-			"name": "5",
-			"src": "Clubs-5.png",
-			"value": 5
-		},
-		{ 
-			"suit": "clubs",
-			"name": "6",
-			"src": "Clubs-6.png",
-			"value": 6
-		},
-		{ 
-			"suit": "clubs",
-			"name": "7",
-			"src": "Clubs-7.png",
-			"value": 7
-		},
-		{ 
-			"suit": "clubs",
-			"name": "8",
-			"src": "Clubs-8.png",
-			"value": 8
-		},
-		{ 
-			"suit": "clubs",
-			"name": "9",
-			"src": "Clubs-9.png",
-			"value": 9
-		},
-		{ 
-			"suit": "clubs",
-			"name": "10",
-			"src": "Clubs-10.png",
-			"value": 10
-		},
-		{ 
-			"suit": "clubs",
-			"name": "ace",
-			"src": "Clubs-Ace.png",
-			"value": 11
-		},
-		{ 
-			"suit": "clubs",
-			"name": "jack",
-			"src": "Clubs-Jack.png",
-			"value": 10
-		},
-		{ 
-			"suit": "clubs",
-			"name": "king",
-			"src": "Clubs-King.png",
-			"value": 10
-		},
-		{ 
-			"suit": "clubs",
-			"name": "queen",
-			"src": "Clubs-Queen.png",
-			"value": 10
-		},
-		{ 
-			"suit": "diamonds",
-			"name": "2",
-			"src": "Diamond-2.png",
-			"value": 2
-		},
-		{ 
-			"suit": "diamonds",
-			"name": "3",
-			"src": "Diamond-3.png",
-			"value": 3
-		},
-		{ 
-			"suit": "diamonds",
-			"name": "4",
-			"src": "Diamond-4.png",
-			"value": 4
-		},
-		{ 
-			"suit": "diamonds",
-			"name": "5",
-			"src": "Diamond-5.png",
-			"value": 5
-		},
-		{ 
-			"suit": "diamonds",
-			"name": "6",
-			"src": "Diamond-6.png",
-			"value": 6
-		},
-		{ 
-			"suit": "diamonds",
-			"name": "7",
-			"src": "Diamond-7.png",
-			"value": 7
-		},
-		{ 
-			"suit": "diamonds",
-			"name": "8",
-			"src": "Diamond-8.png",
-			"value": 8
-		},
-		{ 
-			"suit": "diamonds",
-			"name": "9",
-			"src": "Diamond-9.png",
-			"value": 9
-		},
-		{ 
-			"suit": "diamonds",
-			"name": "10",
-			"src": "Diamond-10.png",
-			"value": 10
-		},
-		{ 
-			"suit": "diamonds",
-			"name": "ace",
-			"src": "Diamond-Ace.png",
-			"value": 11
-		},
-		{ 
-			"suit": "diamonds",
-			"name": "jack",
-			"src": "Diamond-Jack.png",
-			"value": 10
-		},
-		{ 
-			"suit": "diamonds",
-			"name": "king",
-			"src": "Diamond-King.png",
-			"value": 10
-		},
-		{ 
-			"suit": "diamonds",
-			"name": "queen",
-			"src": "Diamond-Queen.png",
-			"value": 10
-		},
-		{ 
-			"suit": "hearts",
-			"name": "2",
-			"src": "Hearts-2.png",
-			"value": 2
-		},
-		{ 
-			"suit": "hearts",
-			"name": "3",
-			"src": "Hearts-3.png",
-			"value": 3
-		},
-		{ 
-			"suit": "hearts",
-			"name": "4",
-			"src": "Hearts-4.png",
-			"value": 4
-		},
-		{ 
-			"suit": "hearts",
-			"name": "5",
-			"src": "Hearts-5.png",
-			"value": 5
-		},
-		{ 
-			"suit": "hearts",
-			"name": "6",
-			"src": "Hearts-6.png",
-			"value": 6
-		},
-		{ 
-			"suit": "hearts",
-			"name": "7",
-			"src": "Hearts-7.png",
-			"value": 7
-		},
-		{ 
-			"suit": "hearts",
-			"name": "8",
-			"src": "Hearts-8.png",
-			"value": 8
-		},
-		{ 
-			"suit": "hearts",
-			"name": "9",
-			"src": "Hearts-9.png",
-			"value": 9
-		},
-		{ 
-			"suit": "hearts",
-			"name": "10",
-			"src": "Hearts-10.png",
-			"value": 10
-		},
-		{ 
-			"suit": "hearts",
-			"name": "ace",
-			"src": "Hearts-Ace.png",
-			"value": 11
-		},
-		{ 
-			"suit": "hearts",
-			"name": "jack",
-			"src": "Hearts-Jack.png",
-			"value": 10
-		},
-		{ 
-			"suit": "hearts",
-			"name": "king",
-			"src": "Hearts-King.png",
-			"value": 10
-		},
-		{ 
-			"suit": "hearts",
-			"name": "queen",
-			"src": "Hearts-Queen.png",
-			"value": 10
-		},
-		{ 
-			"suit": "spades",
-			"name": "2",
-			"src": "Spades-2.png",
-			"value": 2
-		},
-		{ 
-			"suit": "spades",
-			"name": "3",
-			"src": "Spades-3.png",
-			"value": 3
-		},
-		{ 
-			"suit": "spades",
-			"name": "4",
-			"src": "Spades-4.png",
-			"value": 4
-		},
-		{ 
-			"suit": "spades",
-			"name": "5",
-			"src": "Spades-5.png",
-			"value": 5
-		},
-		{ 
-			"suit": "spades",
-			"name": "6",
-			"src": "Spades-6.png",
-			"value": 6
-		},
-		{ 
-			"suit": "spades",
-			"name": "7",
-			"src": "Spades-7.png",
-			"value": 7
-		},
-		{ 
-			"suit": "spades",
-			"name": "8",
-			"src": "Spades-8.png",
-			"value": 8
-		},
-		{ 
-			"suit": "spades",
-			"name": "9",
-			"src": "Spades-9.png",
-			"value": 9
-		},
-		{ 
-			"suit": "spades",
-			"name": "10",
-			"src": "Spades-10.png",
-			"value": 10
-		},
-		{ 
-			"suit": "spades",
-			"name": "ace",
-			"src": "Spades-Ace.png",
-			"value": 11
-		},
-		{ 
-			"suit": "spades",
-			"name": "jack",
-			"src": "Spades-Jack.png",
-			"value": 10
-		},
-		{ 
-			"suit": "spades",
-			"name": "king",
-			"src": "Spades-King.png",
-			"value": 10
-		},
-		{ 
-			"suit": "spades",
-			"name": "queen",
-			"src": "Spades-Queen.png",
-			"value": 10
-		}
-	]
+function createDeck() {
+  var ranks = [
+    { name: '2', value: 2, label: '2', srcIndex: 1 },
+    { name: '3', value: 3, label: '3', srcIndex: 2 },
+    { name: '4', value: 4, label: '4', srcIndex: 3 },
+    { name: '5', value: 5, label: '5', srcIndex: 4 },
+    { name: '6', value: 6, label: '6', srcIndex: 5 },
+    { name: '7', value: 7, label: '7', srcIndex: 6 },
+    { name: '8', value: 8, label: '8', srcIndex: 7 },
+    { name: '9', value: 9, label: '9', srcIndex: 8 },
+    { name: '10', value: 10, label: '10', srcIndex: 9 },
+    { name: 'jack', value: 10, label: 'J', srcIndex: 10 },
+    { name: 'queen', value: 10, label: 'Q', srcIndex: 11 },
+    { name: 'king', value: 10, label: 'K', srcIndex: 12 },
+    { name: 'ace', value: 11, label: 'A', srcIndex: 13 },
+  ];
+
+  var suits = [
+    { name: 'clubs', glyph: '♣' },
+    { name: 'diamonds', glyph: '♦' },
+    { name: 'hearts', glyph: '♥' },
+    { name: 'spades', glyph: '♠' },
+  ];
+
+  var deck = [];
+
+  ranks.forEach(function (rank) {
+    suits.forEach(function (suit) {
+      deck.push({
+        suit: suit.name,
+        name: rank.name,
+        src: 'raiku' + rank.srcIndex + '.png',
+        value: rank.value,
+        label: rank.label,
+        suitGlyph: suit.glyph,
+      });
+    });
+  });
+
+  return deck;
+}
+
+function getCards() {
+  cards = createDeck();
 }
